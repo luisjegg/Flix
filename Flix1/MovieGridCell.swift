9823d450c4fbce6b44bd9f1b0869c86330139c42
luisjegg <41087907+luisjegg@users.noreply.github.com>
2021-02-12 23:17:19 -0800

    Add files via upload

---
+++ Flix1/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Helen Cabrera on 2/12/21.
+//  Copyright © 2021 Luis Carrillo. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
